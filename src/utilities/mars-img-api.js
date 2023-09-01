40c3df32d87e698a4fa57466609227659315510a
Agathe Lavigne <gotgini@gmail.com>
2023-09-01 17:17:02 +0200

    debbuging deploy 2

--- src/utilities/mars-img-api.js
+++ src/utilities/mars-img-api.js
@@ -1,6 +1,6 @@
 import sendRequestNasa from "./send-request-nasa";
 const BASE_URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.REACT_APP_NASA_API_KEY}`;
-console.log('API_KEY', process.env.REACT_APP_NASA_API_KEY)
+
 export async function getMarsImages() {
   return sendRequestNasa(BASE_URL);
 };
